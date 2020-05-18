b69ab494e01a961fa16743faf476c096572b8be9
KyleSun <swy0907163@163.com>
2020-05-18 21:28:45 +0800

    SpringCloud Config和SpringCloud Bus

---
+++ part05_springcloud_config-parent/config-consumer/src/main/java/com/itheima/consumer/controller/OrderController.java
@@ -0,0 +1,34 @@
+package com.itheima.consumer.controller;
+
+
+import com.itheima.consumer.domain.Goods;
+import com.itheima.consumer.feign.GoodsFeignClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RefreshScope
+@RestController
+@RequestMapping("/order")
+public class OrderController {
+
+    @Value("${testKey}")
+    private String testKey;
+
+    @Autowired
+    private GoodsFeignClient goodsFeignClient;
+
+    @GetMapping("/goods/{id}")
+    public Goods findGoodsById(@PathVariable("id") int id) {
+        Goods goods = goodsFeignClient.findGoodsById(id);
+        goods.setTitle(goods.getTitle() + "--" + testKey);
+        return goods;
+    }
+
+
+}
--- part05_springcloud_config-parent/config-server/src/main/java/com/itheima/config/ConfigServerApp.java
+++ part05_springcloud_config-parent/config-server/src/main/java/com/itheima/config/ConfigServerApp.java
@@ -3,6 +3,7 @@ package com.itheima.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @Program: SpringCloud
@@ -12,6 +13,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
  **/
 @SpringBootApplication
 @EnableConfigServer  // 启用config server功能
+@EnableEurekaClient
 public class ConfigServerApp {
     public static void main(String[] args) {
         SpringApplication.run(ConfigServerApp.class, args);
